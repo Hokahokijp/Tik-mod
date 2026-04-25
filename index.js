629b3082ac9c80705fc3229cfa2d09033d9a21bf
Hokahokijp <126981246+Hokahokijp@users.noreply.github.com>
2026-04-25 08:34:00 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,20 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const { WebcastPushConnection } = require('tiktok-live-connector');
+// Di dalam io.on('connection', (socket) => { ... })
+// Tambahkan event ini di bawah event chat, like, dll:
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-// Agar bisa membaca file HTML
-// Agar bisa membaca file utama
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-// TAMBAHKAN INI AGAR PANEL ADMIN BISA DIBUKA
-app.get('/panel-admin', (req, res) => {
-    res.sendFile(__dirname + '/Panel-1990-aa.html');
-});
-
-let tiktokConn;
-
-io.on('connection', (socket) => {
-    socket.on('setTarget', (data) => {
-        if (tiktokConn) tiktokConn.disconnect();
-        
-        tiktokConn = new WebcastPushConnection(data.user);
-        
-        tiktokConn.connect().then(state => {
-            socket.emit('status', 'OK');
-        }).catch(err => {
-            socket.emit('status', 'Gagal! Akun tidak Live atau salah username.');
-        });
-
-        // Kirim data foto dan NICKNAME ke semua yang buka web
-        const sendPhoto = (dataLive) => {
-            io.emit('munculFoto', { 
-                url: dataLive.profilePictureUrl, 
-                nickname: dataLive.nickname, // <-- SEKARANG SUDAH AMBIL NICKNAME
-                uniqueId: dataLive.uniqueId, // <-- TAMBAHAN USERNAME @
-                giftName: dataLive.giftName || null,
-                diamondCount: dataLive.diamondCount || 0,
-                config: data 
-            });
-        };
-
-        tiktokConn.on('chat', sendPhoto);
-        tiktokConn.on('like', sendPhoto);
-        tiktokConn.on('gift', sendPhoto);
-        tiktokConn.on('share', sendPhoto);
-        tiktokConn.on('follow', sendPhoto);
+tiktokConn.on('memberJoin', (dataLive) => {
+    io.emit('userJoin', { 
+        userId: dataLive.userId,
+        url: dataLive.profilePictureUrl, 
+        nickname: dataLive.nickname,
+        config: data 
     });
 });
 
-// Port hosting biasanya dinamis, pakai process.env.PORT
-const PORT = process.env.PORT || 8091;
-server.listen(PORT, () => {
-    console.log('Server berjalan di port ' + PORT);
+tiktokConn.on('roomUserCount', (dataLive) => {
+    // Bisa digunakan untuk update jumlah penonton jika mau
+});
+
+// PENTING: Deteksi saat orang keluar Room
+tiktokConn.on('leave', (dataLive) => {
+    io.emit('userLeave', { userId: dataLive.userId });
 });
