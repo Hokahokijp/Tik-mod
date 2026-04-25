bf0a46e7ee9aeec6feff684d733691b674d89f5d
Hokahokijp <126981246+Hokahokijp@users.noreply.github.com>
2026-04-25 09:40:49 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-// Di dalam io.on('connection', (socket) => { ... })
-// Tambahkan event ini di bawah event chat, like, dll:
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const { WebcastPushConnection } = require('tiktok-live-connector');
 
-tiktokConn.on('memberJoin', (dataLive) => {
-    io.emit('userJoin', { 
-        userId: dataLive.userId,
-        url: dataLive.profilePictureUrl, 
-        nickname: dataLive.nickname,
-        config: data 
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    let tiktokConn;
+
+    socket.on('setTarget', (config) => {
+        if (tiktokConn) tiktokConn.disconnect();
+
+        tiktokConn = new WebcastPushConnection(config.user);
+
+        tiktokConn.connect().then(state => {
+            console.log(`Connected to ${state.roomId}`);
+        }).catch(err => {
+            console.error('Failed to connect', err);
+        });
+
+        tiktokConn.on('gift', (data) => {
+            io.emit('munculFoto', {
+                url: data.profilePictureUrl,
+                nickname: data.nickname,
+                giftName: data.giftName,
+                diamondCount: data.diamondCount,
+                config: config
+            });
+        });
+
+        tiktokConn.on('chat', (data) => {
+            io.emit('chat', {
+                nickname: data.nickname,
+                comment: data.comment
+            });
+        });
+    });
+
+    socket.on('disconnect', () => {
+        if (tiktokConn) tiktokConn.disconnect();
     });
 });
 
-tiktokConn.on('roomUserCount', (dataLive) => {
-    // Bisa digunakan untuk update jumlah penonton jika mau
-});
-
-// PENTING: Deteksi saat orang keluar Room
-tiktokConn.on('leave', (dataLive) => {
-    io.emit('userLeave', { userId: dataLive.userId });
+http.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
