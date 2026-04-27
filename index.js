51caa35021b72a3a4eeecdcff573f9908b04fc1f
Hokahokijp <126981246+Hokahokijp@users.noreply.github.com>
2026-04-28 01:49:38 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -10,54 +10,39 @@ const io = new Server(server);
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); });
 
 let tiktokConn;
 
 io.on('connection', (socket) => {
-    console.log('Browser Terkoneksi ✅');
-
     socket.on('setTarget', (data) => {
         if (tiktokConn) tiktokConn.disconnect();
         
         tiktokConn = new WebcastPushConnection(data.user);
         
-        tiktokConn.connect().then(state => {
-            socket.emit('status', 'Koneksi Sukses!');
-            console.log(`Menghubungkan ke TikTok: ${data.user}`);
+        tiktokConn.connect().then(() => {
+            socket.emit('statusLive', { konek: true, pesan: "Live Terhubung!" });
         }).catch(err => {
-            socket.emit('status', 'Gagal Konek! Akun mungkin offline.');
+            socket.emit('statusLive', { konek: false, pesan: "Live Sedang OFF atau Salah User" });
         });
 
-        // 1. DATA KOMENTAR
+        // Deteksi kalau Host mematikan Live
+        tiktokConn.on('disconnected', () => {
+            io.emit('statusLive', { konek: false, pesan: "Live Berakhir / OFF" });
+        });
+
         tiktokConn.on('chat', (dataLive) => {
-            io.emit('tiktokChat', {
-                user: dataLive.nickname,
-                message: dataLive.comment
-            });
+            io.emit('tiktokChat', { user: dataLive.nickname, message: dataLive.comment });
         });
 
-        // 2. DATA HADIAH (GIFT)
         tiktokConn.on('gift', (dataLive) => {
-            io.emit('tiktokGift', {
-                user: dataLive.nickname,
-                giftName: dataLive.giftName,
-                count: dataLive.repeatCount
-            });
+            io.emit('tiktokGift', { user: dataLive.nickname, giftName: dataLive.giftName });
         });
 
-        // 3. DATA ORANG MASUK (JOIN)
         tiktokConn.on('member', (dataLive) => {
-            io.emit('tiktokJoin', {
-                user: dataLive.nickname
-            });
+            io.emit('tiktokJoin', { user: dataLive.nickname });
         });
     });
 });
 
-const PORT = 8091;
-server.listen(PORT, () => {
-    console.log(`SISTEM AKTIF! Buka http://localhost:${PORT}`);
-});
+server.listen(8091, () => { console.log('Sistem Aktif di Port 8091'); });
