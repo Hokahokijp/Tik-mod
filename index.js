2ef5cfcdb475dbaa510225f0dc91647a1ca6bf55
Hokahokijp <126981246+Hokahokijp@users.noreply.github.com>
2026-04-29 00:08:26 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -16,46 +16,27 @@ io.on('connection', (socket) => {
         if (tiktokConn) tiktokConn.disconnect();
         tiktokConn = new WebcastPushConnection(data.user);
         
-        tiktokConn.connect().then(() => {
-            console.log("Tersambung ke:", data.user);
-            socket.emit('status', 'OK');
-        }).catch(err => {
-            socket.emit('status', 'Gagal!');
-        });
+        tiktokConn.connect().then(() => socket.emit('status', 'OK')).catch(() => socket.emit('status', 'Gagal'));
 
-        // 1. TANGKAP JOIN (Foto profil muncul)
+        // JOIN & LIKE (Melayang sebagai bubble)
         tiktokConn.on('member', (dataLive) => {
-            io.emit('event_visual', {
-                type: 'join',
-                user: dataLive.nickname,
-                img: dataLive.profilePictureUrl
-            });
+            io.emit('event_visual', { type: 'join', user: dataLive.nickname, img: dataLive.profilePictureUrl });
         });
 
-        // 2. TANGKAP LIKE (Tap-tap layar muncul hati)
         tiktokConn.on('like', (dataLive) => {
-            io.emit('event_visual', {
-                type: 'like',
-                user: dataLive.nickname,
-                img: dataLive.profilePictureUrl
-            });
+            io.emit('event_visual', { type: 'like', user: dataLive.nickname, img: dataLive.profilePictureUrl });
         });
 
-        // 3. TANGKAP CHAT (Google bacain komentar)
         tiktokConn.on('chat', (dataLive) => {
-            io.emit('chat_masuk', { 
-                user: dataLive.nickname, 
-                text: dataLive.comment 
-            });
+            io.emit('chat_masuk', { user: dataLive.nickname, text: dataLive.comment });
         });
 
-        // 4. TANGKAP GIFT (Slider & Kartu Cek Khodam)
+        // GIFT (Masuk Slider + Kartu Cek Khodam)
         tiktokConn.on('gift', (dataLive) => {
-            let hasil = ""; 
-            let tipe = "";
+            let hasil = ""; let tipe = "";
             const g = dataLive.giftName; 
             const c = dataLive.repeatCount;
-            const d = dataLive.diamondCount; // Koin per gift
+            const d = dataLive.diamondCount;
 
             if (g === 'Rose') {
                 tipe = "CEK JODOH";
@@ -63,33 +44,20 @@ io.on('connection', (socket) => {
             } else if (g === 'GG') {
                 tipe = "CEK KHODAM";
                 hasil = c >= 10 ? "Khodam Macan Sakti Prabu Siliwangi!" : "Khodam Kucing Putih.";
-            } else if (g === 'Donut') {
-                tipe = "CEK REZEKI";
-                hasil = c >= 5 ? "Rezeki nomplok segera datang!" : "Rezeki lancar terus.";
+            } else {
+                tipe = "GIFT";
+                hasil = "Terima kasih atas hadiahnya!";
             }
 
-            // Kirim data ke HTML untuk Kartu & Slider
-            if (tipe !== "") {
-                io.emit('kartu_cek', {
-                    img: dataLive.profilePictureUrl,
-                    user: dataLive.nickname,
-                    tipe: tipe,
-                    hasil: hasil,
-                    diamonds: d * c // TOTAL KOIN buat urutan slider
-                });
-            } else {
-                // Kalo gift lain (bukan buat cek khodam) tetep masuk slider
-                io.emit('kartu_cek', {
-                    img: dataLive.profilePictureUrl,
-                    user: dataLive.nickname,
-                    tipe: "GIFT",
-                    hasil: "Terima kasih kadonya!",
-                    diamonds: d * c
-                });
-            }
+            io.emit('kartu_cek', {
+                img: dataLive.profilePictureUrl,
+                user: dataLive.nickname,
+                tipe: tipe,
+                hasil: hasil,
+                diamonds: d * c // Kirim total koin buat slider
+            });
         });
     });
 });
 
-const PORT = 8091;
-server.listen(PORT, () => console.log(`Server jalan di http://localhost:${PORT}`));
+server.listen(8091, () => console.log('Server Ready Port 8091'));
