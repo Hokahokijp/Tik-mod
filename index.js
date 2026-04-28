f747ebb85c1ff8500f3a6f52399fc9383d16d969
Hokahokijp <126981246+Hokahokijp@users.noreply.github.com>
2026-04-28 23:01:37 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,59 +5,38 @@ const { WebcastPushConnection } = require('tiktok-live-connector');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server);
 
 app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); });
 
 let tiktokConn;
+let targetUser = "";
+
+function connectTikTok(username) {
+    if (tiktokConn) {
+        tiktokConn.removeAllListeners();
+        tiktokConn.disconnect();
+    }
+    targetUser = username;
+    tiktokConn = new WebcastPushConnection(username);
+
+    tiktokConn.connect().then(state => {
+        console.log(`BISMILLAH GACOR: ${username}`);
+    }).catch(err => {
+        setTimeout(() => connectTikTok(targetUser), 3000);
+    });
+
+    tiktokConn.on('member', (data) => io.emit('memberJoin', data));
+    tiktokConn.on('chat', (data) => io.emit('chat', data));
+    tiktokConn.on('gift', (data) => {
+        io.emit('munculFoto', { ...data, url: data.profilePictureUrl });
+    });
+    tiktokConn.on('leave', (data) => io.emit('memberLeave', { uniqueId: data.uniqueId }));
+    tiktokConn.on('disconnected', () => setTimeout(() => connectTikTok(targetUser), 2000));
+}
 
 io.on('connection', (socket) => {
-    socket.on('setTarget', (data) => {
-        if (tiktokConn) tiktokConn.disconnect();
-        tiktokConn = new WebcastPushConnection(data.user);
-        
-        tiktokConn.connect().then(() => {
-            socket.emit('status', 'CONNECTED');
-        }).catch(() => {
-            socket.emit('status', 'ERROR');
-        });
-
-        // TANGKAP KOMENTAR - LANGSUNG LEMPAR KE FRONTEND
-        tiktokConn.on('chat', (dataLive) => {
-            io.emit('chat_masuk', {
-                user: dataLive.nickname,
-                text: dataLive.comment
-            });
-        });
-
-        // TANGKAP GIFT - CEK KHODAM
-        tiktokConn.on('gift', (dataLive) => {
-            let hasil = "";
-            let tipe = "";
-            const g = dataLive.giftName;
-            const c = dataLive.repeatCount;
-
-            if (g === 'Rose') {
-                tipe = "CEK JODOH";
-                hasil = c >= 10 ? "Jodohmu inisial A, orangnya setia banget!" : "Jodohmu sudah sangat dekat.";
-            } else if (g === 'GG') {
-                tipe = "CEK KHODAM";
-                hasil = c >= 10 ? "Khodam Macan Sakti Prabu Siliwangi!" : "Khodam Kucing Putih Lucu.";
-            } else if (g === 'Donut') {
-                tipe = "CEK REZEKI";
-                hasil = c >= 5 ? "Rezeki nomplok bakal datang besok pagi!" : "Rezeki lancar kayak air.";
-            }
-
-            if (tipe) {
-                io.emit('kartu_cek', {
-                    img: dataLive.profilePictureUrl,
-                    user: dataLive.nickname,
-                    tipe: tipe,
-                    hasil: hasil
-                });
-            }
-        });
-    });
+    socket.on('setTarget', (data) => connectTikTok(data.user));
 });
 
-server.listen(8091, () => { console.log('Server Tikfinity-Style 8091'); });
+server.listen(8091, () => { console.log('SERVER BUBBLE CHAT READY DI 8091'); });
