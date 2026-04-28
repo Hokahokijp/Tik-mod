f7ab034c5fa5dbb62355e6f1e2588c434a57fb39
Hokahokijp <126981246+Hokahokijp@users.noreply.github.com>
2026-04-28 23:23:35 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -10,33 +10,60 @@ const io = new Server(server);
 app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); });
 
 let tiktokConn;
-let targetUser = "";
-
-function connectTikTok(username) {
-    if (tiktokConn) {
-        tiktokConn.removeAllListeners();
-        tiktokConn.disconnect();
-    }
-    targetUser = username;
-    tiktokConn = new WebcastPushConnection(username);
-
-    tiktokConn.connect().then(state => {
-        console.log(`BISMILLAH GACOR: ${username}`);
-    }).catch(err => {
-        setTimeout(() => connectTikTok(targetUser), 3000);
-    });
-
-    tiktokConn.on('member', (data) => io.emit('memberJoin', data));
-    tiktokConn.on('chat', (data) => io.emit('chat', data));
-    tiktokConn.on('gift', (data) => {
-        io.emit('munculFoto', { ...data, url: data.profilePictureUrl });
-    });
-    tiktokConn.on('leave', (data) => io.emit('memberLeave', { uniqueId: data.uniqueId }));
-    tiktokConn.on('disconnected', () => setTimeout(() => connectTikTok(targetUser), 2000));
-}
 
 io.on('connection', (socket) => {
-    socket.on('setTarget', (data) => connectTikTok(data.user));
+    socket.on('setTarget', (data) => {
+        if (tiktokConn) tiktokConn.disconnect();
+        tiktokConn = new WebcastPushConnection(data.user);
+        
+        tiktokConn.connect().then(() => socket.emit('status', 'OK'));
+
+        // 1. TANGKAP JOIN (Member Baru Masuk)
+        tiktokConn.on('member', (dataLive) => {
+            io.emit('event_visual', {
+                type: 'join',
+                user: dataLive.nickname,
+                img: dataLive.profilePictureUrl
+            });
+        });
+
+        // 2. TANGKAP LIKE (Tap-tap Layar)
+        tiktokConn.on('like', (dataLive) => {
+            io.emit('event_visual', {
+                type: 'like',
+                user: dataLive.nickname,
+                img: dataLive.profilePictureUrl
+            });
+        });
+
+        // 3. TANGKAP CHAT (Komentar)
+        tiktokConn.on('chat', (dataLive) => {
+            io.emit('chat_masuk', { user: dataLive.nickname, text: dataLive.comment });
+        });
+
+        // 4. TANGKAP GIFT (Cek Khodam)
+        tiktokConn.on('gift', (dataLive) => {
+            let hasil = ""; let tipe = "";
+            const g = dataLive.giftName; const c = dataLive.repeatCount;
+
+            if (g === 'Rose') {
+                tipe = "CEK JODOH";
+                hasil = c >= 10 ? "Jodohmu inisial A, setia banget!" : "Jodohmu sudah dekat.";
+            } else if (g === 'GG') {
+                tipe = "CEK KHODAM";
+                hasil = c >= 10 ? "Khodam Macan Sakti Prabu Siliwangi!" : "Khodam Kucing Putih.";
+            }
+
+            if (tipe) {
+                io.emit('kartu_cek', {
+                    img: dataLive.profilePictureUrl,
+                    user: dataLive.nickname,
+                    tipe: tipe,
+                    hasil: hasil
+                });
+            }
+        });
+    });
 });
 
-server.listen(8091, () => { console.log('SERVER BUBBLE CHAT READY DI 8091'); });
+server.listen(8091, () => console.log('Server Ready Port 8091'));
